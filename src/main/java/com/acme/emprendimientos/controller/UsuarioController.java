8b9c60f1a31d198706d5cf9b08fc88aefcfd0bcd
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-12 14:40:06 -0300

    added delte by id for usuario

--- src/main/java/com/acme/emprendimientos/controller/UsuarioController.java
+++ src/main/java/com/acme/emprendimientos/controller/UsuarioController.java
@@ -2,13 +2,16 @@ package com.acme.emprendimientos.controller;
 
 import com.acme.emprendimientos.entity.Usuario;
 import com.acme.emprendimientos.repository.UsuarioRepository;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/usuario")
@@ -23,6 +26,18 @@ public class UsuarioController {
     @GetMapping
     public List<Usuario> getUsuarios(){
         return usuarioRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public Map<String, Boolean> deleteUsuario(@PathVariable(value = "id") Long Id)
+            throws ResourceNotFoundException {
+        Usuario usuario = usuarioRepository.findById(Id)
+                .orElseThrow(() -> new ResourceNotFoundException("No existe un usuario con el id: " + Id));
+
+        usuarioRepository.delete(usuario);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 
     @PostMapping
