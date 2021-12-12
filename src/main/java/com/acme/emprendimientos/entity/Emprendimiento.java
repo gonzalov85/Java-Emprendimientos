8e996bdf23662c95eb4e169a26cb17648ad58ecc
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-12 01:22:18 -0300

    added emprendimiento entity

--- src/main/java/com/acme/emprendimientos/entity/Emprendimiento.java
+++ src/main/java/com/acme/emprendimientos/entity/Emprendimiento.java
@@ -1,7 +1,100 @@
 package com.acme.emprendimientos.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 public class Emprendimiento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String descripcion;
+
+    private String contenido;
+
+    private LocalDate fechaCreacion;
+
+    private BigDecimal objetivo;
+
+    private boolean publicado;
+
+    private List<String> capturas;
+
+    private List<String> tags;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public BigDecimal getObjetivo() {
+        return objetivo;
+    }
+
+    public void setObjetivo(BigDecimal objetivo) {
+        this.objetivo = objetivo;
+    }
+
+    public boolean isPublicado() {
+        return publicado;
+    }
+
+    public void setPublicado(boolean publicado) {
+        this.publicado = publicado;
+    }
+
+    public List<String> getCapturas() {
+        return capturas;
+    }
+
+    public void setCapturas(List<String> capturas) {
+        this.capturas = capturas;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
 }
