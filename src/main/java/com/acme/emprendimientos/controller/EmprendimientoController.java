2aa9c167e00f7e727dfa7572e88e7088965ec075
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-12 14:48:05 -0300

    added delete by id for emprendimiento

--- src/main/java/com/acme/emprendimientos/controller/EmprendimientoController.java
+++ src/main/java/com/acme/emprendimientos/controller/EmprendimientoController.java
@@ -3,13 +3,16 @@ package com.acme.emprendimientos.controller;
 import com.acme.emprendimientos.entity.Emprendimiento;
 import com.acme.emprendimientos.entity.Usuario;
 import com.acme.emprendimientos.repository.EmprendimientoRepository;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/emprendimiento")
@@ -24,6 +27,18 @@ public class EmprendimientoController {
     @GetMapping
     public List<Emprendimiento> getEmprendimientos(){
         return emprendimientoRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public Map<String, Boolean> deleteEmprendimiento(@PathVariable(value = "id") Long Id)
+            throws ResourceNotFoundException {
+        Emprendimiento emprendimiento = emprendimientoRepository.findById(Id)
+                .orElseThrow(() -> new ResourceNotFoundException("No existe un Emprendimiento con el id: " + Id));
+
+        emprendimientoRepository.delete(emprendimiento);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 
     @PostMapping
