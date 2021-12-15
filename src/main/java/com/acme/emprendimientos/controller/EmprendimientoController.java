a8da0c8c347106c316cf05a98ee7441a94a72bb1
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-15 04:41:57 -0300

    evento controller

--- src/main/java/com/acme/emprendimientos/controller/EmprendimientoController.java
+++ src/main/java/com/acme/emprendimientos/controller/EmprendimientoController.java
@@ -1,11 +1,12 @@
 package com.acme.emprendimientos.controller;
 
-import com.acme.emprendimientos.dto.OperacionEmprendimiento;
+import com.acme.emprendimientos.dto.EventoDTO;
 import com.acme.emprendimientos.entity.Emprendimiento;
 
 import com.acme.emprendimientos.repository.EmprendimientoRepository;
+import com.acme.emprendimientos.repository.EventoRepository;
 import com.acme.emprendimientos.service.EmprendimientoService;
-import org.apache.velocity.exception.ResourceNotFoundException;
+import com.acme.emprendimientos.service.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,47 +14,53 @@ import org.springframework.web.bind.annotation.*;
 
 
 import javax.validation.Valid;
-import java.util.HashMap;
 
-import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/emprendimiento")
 public class EmprendimientoController {
-
-    private final EmprendimientoService emprendimientoService;
     private final EmprendimientoRepository emprendimientoRepository;
-
+    private final EmprendimientoService emprendimientoService;
+    private final EventoRepository eventoRepository;
+    private final EventoService eventoService;
     @Autowired
-    public EmprendimientoController(EmprendimientoService emprendimientoService, EmprendimientoRepository emprendimientoRepository) {
-        this.emprendimientoService = emprendimientoService;
+    public EmprendimientoController(EmprendimientoRepository emprendimientoRepository,
+                                    EmprendimientoService emprendimientoService,
+                                    EventoRepository eventoRepository,
+                                    EventoService eventoService) {
         this.emprendimientoRepository = emprendimientoRepository;
+        this.emprendimientoService = emprendimientoService;
+        this.eventoRepository = eventoRepository;
+        this.eventoService = eventoService;
     }
 
-
-    @PostMapping
-    public ResponseEntity<?> createEmprendimiento(@Valid @RequestBody OperacionEmprendimiento operacionEmprendimiento) {
-
-        return new ResponseEntity<>(emprendimientoService.createEmprendimiento(operacionEmprendimiento), HttpStatus.CREATED);
+    @PostMapping(value = "/usuarios/{id}/emprendimientos")
+    public ResponseEntity<?> crearEmprendimiento(@PathVariable("id") Long usuarioId,
+                                                 @Valid @RequestBody Emprendimiento emprendimiento) {
+        return new ResponseEntity<>(emprendimientoService.guardar(usuarioId, emprendimiento), HttpStatus.CREATED);
     }
-
-    //GET All Emprendimientos
-    @GetMapping
-    public ResponseEntity<?> getEmprendimientos() {
-        return new ResponseEntity<>(emprendimientoRepository.findAll(), HttpStatus.OK);
+    @PutMapping(value = "/emprendimientos/{id}/quitar")
+    public Emprendimiento eliminarEmprendimiento(@PathVariable("id") Long id, Emprendimiento emprendimiento) {
+        return this.emprendimientoService.eliminar(id, emprendimiento);
     }
-
-    //Delete Emprendimiento by id
-    @DeleteMapping("/{id}")
-    public Map<String, Boolean> deleteEmprendimiento(@PathVariable(value = "id") Long Id)
-            throws ResourceNotFoundException {
-        Emprendimiento emprendimiento = emprendimientoRepository.findById(Id)
-                .orElseThrow(() -> new ResourceNotFoundException("No existe un Emprendimiento con el id: " + Id));
-
-        emprendimientoRepository.delete(emprendimiento);
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("deleted", Boolean.TRUE);
-        return response;
+    @PutMapping(value = "/emprendimientos/{id}")
+    public Emprendimiento modificarEmprendimiento(@PathVariable("id") Long id,
+                                                  @Valid @RequestBody Emprendimiento emprendimiento) {
+        return this.emprendimientoService.modificar(id, emprendimiento);
     }
-
+    @GetMapping(value = "/emprendimientos")
+    public ResponseEntity<?> obtenerTodosLosEmprendimientos(
+            @RequestParam(name = "nombre", required = false) String nombre) {
+        return new ResponseEntity<>(emprendimientoService.obtenerTodos(nombre) ,HttpStatus.OK);
+    }
+    @GetMapping(value = "/emprendimientos/no_publicados")
+    public ResponseEntity<?> obtenerEmprendimientosNoPublicados() {
+        return new ResponseEntity<>(emprendimientoService.obtenerNoPublicados(), HttpStatus.OK);
+    }
+    @PostMapping(value = "/emprendimientos/{emprendimientoId}/eventos/{eventoId}")
+    public ResponseEntity<?> registrarEvento(@PathVariable("emprendimientoId") Long emprendimientoId,
+                                             @PathVariable("eventoId") Long eventoId, EventoDTO eventoDTO) {
+        emprendimientoRepository.findById(emprendimientoId);
+        eventoRepository.findById(eventoId);
+        return new ResponseEntity<>(eventoService.registrar(emprendimientoId, eventoId, eventoDTO), HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/acme/emprendimientos/controller/EventoController.java
+++ src/main/java/com/acme/emprendimientos/controller/EventoController.java
@@ -1,4 +1,42 @@
 package com.acme.emprendimientos.controller;
 
+import com.acme.emprendimientos.entity.Evento;
+import com.acme.emprendimientos.service.EventoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/eventos")
 public class EventoController {
+    private final EventoService eventoService;
+
+    @Autowired
+    public EventoController(EventoService eventoService) {
+        this.eventoService = eventoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> crearEvento(@Valid @RequestBody Evento evento) {
+        return new ResponseEntity<>(eventoService.guardar(evento), HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{id}/eliminar")
+    public Evento eliminarEvento(@PathVariable("id") Long id, Evento evento) {
+        return this.eventoService.eliminar(id, evento);
+    }
+
+    @PutMapping(value = "/actualizar-estado")
+    public void actualizarEvento() {
+        this.eventoService.actualizar();
+    }
+
+    @GetMapping(value = "/{id}/ranking")
+    public ResponseEntity<?> rankingDelEvento(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(eventoService.rankear(id), HttpStatus.OK);
+    }
 }
+
