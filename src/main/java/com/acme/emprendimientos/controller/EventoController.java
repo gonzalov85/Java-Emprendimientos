2abb88a90407e8777ad5e222901fe5f71b7ca920
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-15 04:14:42 -0300

    added voto controller

---
+++ src/main/java/com/acme/emprendimientos/controller/EventoController.java
@@ -0,0 +1,4 @@
+package com.acme.emprendimientos.controller;
+
+public class EventoController {
+}
---
+++ src/main/java/com/acme/emprendimientos/controller/VotoController.java
@@ -0,0 +1,27 @@
+package com.acme.emprendimientos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "votos")
+public class VotoController {
+    private final VotoService votoService;
+    @Autowired
+    public VotoController(VotoService votoService) {
+        this.votoService = votoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> crearVoto(@Valid @RequestBody VotoDTO votoDTO) {
+        return new ResponseEntity<>(votoService.crearVoto(votoDTO), HttpStatus.CREATED);
+    }
+    @GetMapping(value = "/{usuarioId}")
+    public ResponseEntity<?> obtenerLosVotosDeUnUsuario(@PathVariable("usuarioId") Long usuarioId) {
+        return new ResponseEntity<>(votoService.obtenerVotos(usuarioId), HttpStatus.OK);
+    }
+} 
