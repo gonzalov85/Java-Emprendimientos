b8ec5db1acd7a8229d0fbdf43ba05ac4f2b78b4c
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-15 04:22:58 -0300

    add evento service

---
+++ src/main/java/com/acme/emprendimientos/dto/EventoDTO.java
@@ -0,0 +1,20 @@
+package com.acme.emprendimientos.dto;
+
+
+public class EventoDTO {
+    private Long eventoId;
+    private Long suscriptorId;
+
+    public Long getEventId() {
+        return eventoId;
+    }
+    public void setEventId(Long eventId) {
+        this.eventoId = eventId;
+    }
+    public Long getSuscriptorId() {
+        return suscriptorId;
+    }
+    public void setSuscriptorId(Long suscriptorId) {
+        this.suscriptorId = suscriptorId;
+    }
+}
